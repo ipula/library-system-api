6d01aa79cd09b2f04ee073bc7f706cef5a66380d
ipula <ipularanasinghe007@gmail.com>
2026-01-25 03:41:31 +0100

    chnage entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,12 +3,14 @@ set -e
 
 echo "Starting Laravel..."
 
-# Fix permissions
-chown -R www-data:www-data /var/www/html
+# Fix permissions (DO NOT touch .env)
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Cache
+# Clear & cache config
 php artisan config:clear
 php artisan config:cache
+
+echo "Laravel ready."
 
 exec php-fpm
